2f7e95573ffb2ee45ba658e7a4cb74adbf01c138
LitheshSari <1513503320@qq.com>
2022-12-03 13:58:46 +0800

    Update 20221203 1. Fix a redundant thread in sentry_communicator

--- sp_control/sentry_communicator/src/sentry_communicator.cpp
+++ sp_control/sentry_communicator/src/sentry_communicator.cpp
@@ -5,14 +5,9 @@ int main(int argc, char **argv)
     ros::init(argc, argv, "sentry_communicator");
     ros::NodeHandle nh;
 
-    sentry_communicator::CanBus can_bus("can0", 0, nh);
+    sentry_communicator::CanBus can_bus("can0", 20, nh);
     ros::AsyncSpinner spinner(2);
     spinner.start();
 
-    while (ros::ok())
-    {
-        can_bus.write();
-        ros::spinOnce();
-    }
-    return 0;
+    ros::waitForShutdown();
 }
