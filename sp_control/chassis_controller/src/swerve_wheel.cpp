ba9223b02a4783d987f0bfb191e5b03b153b16a4
Huan <839003509@QQ.COM>
2022-12-16 21:19:06 +0800

    更改了运动学部分的问题，现在底盘可以正常运动了

--- sp_control/chassis_controller/src/swerve_wheel.cpp
+++ sp_control/chassis_controller/src/swerve_wheel.cpp
@@ -48,20 +48,19 @@ namespace chassis_controller
         half_wheel_track_  = wheel_track_/2;
         half_wheel_base_   = wheel_base_/2;
 
-        para_xy = pow(vel_cmd_.x, 2) + pow(vel_cmd_.y, 2) + pow((half_wheel_base_/2) * vel_cmd_.z, 2) + pow((half_wheel_track_/2) * vel_cmd_.z, 2);
-        para_x = half_wheel_track_/2  * vel_cmd_.z * vel_cmd_.x; 
-        para_y = half_wheel_base_/2 * vel_cmd_.z * vel_cmd_.y;
+        para_xy = pow(vel_cmd_.x, 2) + pow(vel_cmd_.y, 2) + pow(half_wheel_base_ * vel_cmd_.z, 2) + pow(half_wheel_track_ * vel_cmd_.z, 2);
+        para_x = wheel_track_  * vel_cmd_.z * vel_cmd_.x; 
+        para_y = wheel_base_ * vel_cmd_.z * vel_cmd_.y;
 
-        //遥控器无值时直接维持上次的值
-        if (vel_cmd_.x==0 && vel_cmd_.y==0 && vel_cmd_.z==0)
-                return;
+        // //遥控器无值时直接维持上次的值
+        // if (vel_cmd_.x==0 && vel_cmd_.y==0 && vel_cmd_.z==0)
+        //         return;
 
-	float test__ = -sqrt(para_xy - para_x + para_y)/wheel_radius_;
         //注意电机安装方向的不同
-        ctrl_rf_driving_.setCommand(-sqrt(para_xy - para_x + para_y)/wheel_radius_);
-        ctrl_lf_driving_.setCommand(-sqrt(para_xy - para_x - para_y)/wheel_radius_);
-        ctrl_lb_driving_.setCommand(-sqrt(para_xy + para_x - para_y)/wheel_radius_);
-        ctrl_rb_driving_.setCommand(-sqrt(para_xy + para_x + para_y)/wheel_radius_);
+        ctrl_rf_driving_.setCommand(sqrt(para_xy + para_x + para_y)/wheel_radius_);
+        ctrl_lf_driving_.setCommand(sqrt(para_xy - para_x + para_y)/wheel_radius_);
+        ctrl_lb_driving_.setCommand(sqrt(para_xy - para_x - para_y)/wheel_radius_);
+        ctrl_rb_driving_.setCommand(sqrt(para_xy + para_x - para_y)/wheel_radius_);
 
         ctrl_lf_driving_.update(time, period);
         ctrl_rf_driving_.update(time, period);
@@ -69,18 +68,39 @@ namespace chassis_controller
         ctrl_rb_driving_.update(time, period);
 
         ROS_INFO_STREAM("x:"<<vel_cmd_.x<<" y:"<<vel_cmd_.y<<" z:"<<vel_cmd_.z);
-        ROS_INFO_STREAM(test__);
+ 
+
 
         //解算轮角度，atan2(y, x) = atan (y/x)
-        ctrl_rf_heading_.setCommand(atan2(vel_cmd_.y + half_wheel_base_ * vel_cmd_.z, vel_cmd_.x - half_wheel_track_* vel_cmd_.z));
-        ctrl_lf_heading_.setCommand(atan2(vel_cmd_.y - half_wheel_base_ * vel_cmd_.z, vel_cmd_.x - half_wheel_track_* vel_cmd_.z));
-        ctrl_lb_heading_.setCommand(atan2(vel_cmd_.y - half_wheel_base_ * vel_cmd_.z, vel_cmd_.x + half_wheel_track_* vel_cmd_.z));
-        ctrl_rb_heading_.setCommand(atan2(vel_cmd_.y + half_wheel_base_ * vel_cmd_.z, vel_cmd_.x + half_wheel_track_* vel_cmd_.z));
+        float angle[4];
+        angle[0]=atan2(vel_cmd_.y + half_wheel_base_ * vel_cmd_.z, vel_cmd_.x + half_wheel_track_* vel_cmd_.z);
+        angle[1]=atan2(vel_cmd_.y + half_wheel_base_ * vel_cmd_.z, vel_cmd_.x - half_wheel_track_* vel_cmd_.z);
+        angle[2]=atan2(vel_cmd_.y - half_wheel_base_ * vel_cmd_.z, vel_cmd_.x - half_wheel_track_* vel_cmd_.z);
+        angle[3]=atan2(vel_cmd_.y - half_wheel_base_ * vel_cmd_.z, vel_cmd_.x + half_wheel_track_* vel_cmd_.z);
+        // for(int i=0;i<4;i++)
+        // {
+        //     if(angle[i]<-1.07)
+        //         angle[i]+=3.14;
+
+        // }
+
+
+        // ctrl_rf_heading_.setCommand(atan2(vel_cmd_.y + half_wheel_base_ * vel_cmd_.z, vel_cmd_.x - half_wheel_track_* vel_cmd_.z));
+        // ctrl_lf_heading_.setCommand(atan2(vel_cmd_.y - half_wheel_base_ * vel_cmd_.z, vel_cmd_.x - half_wheel_track_* vel_cmd_.z));
+        // ctrl_lb_heading_.setCommand(atan2(vel_cmd_.y - half_wheel_base_ * vel_cmd_.z, vel_cmd_.x + half_wheel_track_* vel_cmd_.z));
+        // ctrl_rb_heading_.setCommand(atan2(vel_cmd_.y + half_wheel_base_ * vel_cmd_.z, vel_cmd_.x + half_wheel_track_* vel_cmd_.z));
+        
+        ctrl_rf_heading_.setCommand(angle[0]);
+        ctrl_lf_heading_.setCommand(angle[1]);
+        ctrl_lb_heading_.setCommand(angle[2]);
+        ctrl_rb_heading_.setCommand(angle[3]);
+
 
         ctrl_lf_heading_.update(time, period);
         ctrl_rf_heading_.update(time, period);
         ctrl_lb_heading_.update(time, period);
         ctrl_rb_heading_.update(time, period);
+        ROS_INFO_STREAM(" rf_ang:"<<angle[0]<<" lf_ang:"<<angle[1]<<" lb_ang:"<<angle[2]<<" rb_ang:"<<angle[3]);
     }
 
     geometry_msgs::Twist SwerveWheel::forwardKinematics()
@@ -99,9 +119,9 @@ namespace chassis_controller
         double rb_velocity = ctrl_rb_driving_.joint_.getVelocity()*wheel_radius_;
         double rb_angle    = ctrl_rb_heading_.joint_.getPosition();
 
-        vel_data.linear.x   = (rb_velocity*cos(rb_angle)+lf_velocity*cos(lf_angle))/2;
-        vel_data.linear.y   = (rb_velocity*sin(rb_angle)+lf_velocity*sin(lf_angle))/2;
-        vel_data.angular.z  = (rb_velocity*cos(rb_angle)-lf_velocity*cos(lf_angle))/(2*wheel_track_);
+        vel_data.linear.x   = (rf_velocity*cos(rf_angle)+lb_velocity*cos(lb_angle))/2;
+        vel_data.linear.y   = (rf_velocity*sin(rf_angle)+lb_velocity*sin(lb_angle))/2;
+        vel_data.angular.z  = (rf_velocity*sin(rf_angle)-lb_velocity*sin(lb_angle))/wheel_base_;
         return vel_data;
     }
     PLUGINLIB_EXPORT_CLASS(chassis_controller::SwerveWheel, controller_interface::ControllerBase)
