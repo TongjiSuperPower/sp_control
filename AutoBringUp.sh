b92b9e813419f022e35362f3372d7b7eed3f5528
lyc12569 <2058362643@qq.com>
2023-05-30 15:37:16 +0800

    AutoBringUp.sh

--- AutoBringUp.sh
+++ AutoBringUp.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+source /opt/ros/noetic/setup.bash
+source ~/catkin_ws/devel/setup.bash
 
-sudo ip link set can0 up type can bitrate 1000000
-sudo ip link set can1 up type can bitrate 1000000
-sudo ip link set can2 up type can bitrate 1000000
+sudo ip link set can3 up type can bitrate 1000000
+sudo ip link set can4 up type can bitrate 1000000
+sudo ip link set can5 up type can bitrate 1000000
+sudo chmod 777 /dev/ttyUSB0
 
+sleep 1s
+gnome-terminal --tab "engineer-control" -- bash -c "roslaunch sp_hw load_engineer.launch;exec bash;"
+
+sleep 10s
+{
+gnome-terminal --tab "manipulator-control" -- bash -c "source ~/catkin_ws/devel/setup.bash;rosrun manipulator_control manipulator_control;exec bash;"
+}&
