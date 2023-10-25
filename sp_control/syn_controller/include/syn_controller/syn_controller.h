35fe06c18baa2f55c866a8d1fb725ddba04fbc9f
Cherryblossomnight <kclroku@outlook.com>
2023-10-25 17:57:22 +0800

    新增syn_controller，用作控制电机同步运动

---
+++ sp_control/syn_controller/include/syn_controller/syn_controller.h
@@ -0,0 +1,53 @@
+//
+// Created by Cherryblossomnight on 2023/10/24.
+//
+
+#pragma once
+
+#include <controller_interface/controller.h>
+#include <controller_interface/multi_interface_controller.h>
+#include <realtime_tools/realtime_publisher.h>
+#include <hardware_interface/joint_command_interface.h>
+#include <effort_controllers/joint_position_controller.h>
+#include <pluginlib/class_list_macros.hpp>
+#include <std_msgs/Float64.h>
+namespace syn_controller
+{
+  class SynController : public controller_interface::MultiInterfaceController<hardware_interface::EffortJointInterface,
+                                                          hardware_interface::JointStateInterface>
+  {
+  public:
+    SynController() = default;
+
+    bool init(hardware_interface::RobotHW *robot_hw, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh);
+
+    void update(const ros::Time &time, const ros::Duration &period) override;
+
+    double getPosition();
+    
+    void setCommand(double cmd);
+
+      
+
+  private:
+    void initPos();
+
+    void setCommandCB(const std_msgs::Float64::ConstPtr &msg);
+
+    ros::NodeHandle nh;
+    ros::Subscriber syn_cmd_sub_;
+
+ 
+
+    hardware_interface::EffortJointInterface *effort_joint_interface_{};
+
+    effort_controllers::JointPositionController ctrl_left_;
+    effort_controllers::JointPositionController ctrl_right_; 
+
+    double pos_{};
+    double cmd_{};
+
+    bool has_feedforward_{};
+    double feedforward_{};
+  };
+} // namespace syn_controller
---
+++ sp_control/syn_controller/src/syn_controller.cpp
@@ -0,0 +1,68 @@
+//
+// Created by CherryBlossomNight on 2023/10/24.
+//
+
+#include "syn_controller/syn_controller.h"
+#include <pluginlib/class_list_macros.hpp>
+#include <sp_common/base_utilities.h>
+
+namespace syn_controller
+{
+  bool SynController::init(hardware_interface::RobotHW *robot_hw, ros::NodeHandle &root_nh, ros::NodeHandle &controller_nh)
+  {
+    ros::NodeHandle nh_left = ros::NodeHandle(controller_nh, "left_joint");
+    ros::NodeHandle nh_right = ros::NodeHandle(controller_nh, "right_joint");
+    syn_cmd_sub_ = nh.subscribe<std_msgs::Float64>("syn_cmd", 1, &SynController::setCommandCB, this);
+
+    has_feedforward_ = sp_common::getParam(controller_nh, "has_feedforward", false);
+    feedforward_ = sp_common::getParam(controller_nh, "feedforward", 0.0);
+
+    effort_joint_interface_ = robot_hw->get<hardware_interface::EffortJointInterface>();
+    if (!ctrl_left_.init(effort_joint_interface_, nh_left) || !ctrl_right_.init(effort_joint_interface_, nh_right) )
+        return false;
+
+
+    ROS_INFO("SYN : Initializing Completed");
+
+    
+    return true;
+  }
+
+  void SynController::update(const ros::Time &time, const ros::Duration &period)
+  { 
+    ROS_INFO_STREAM("LEFT_POS: " << ctrl_left_.joint_.getPosition());
+    ROS_INFO_STREAM("RIGHT_POS: " << ctrl_right_.joint_.getPosition());
+    pos_ = (ctrl_left_.joint_.getPosition() + ctrl_right_.joint_.getPosition()) / 2;
+    //if (ctrl_left_.joint_.getPosition() - ctrl_right_.joint_.getPosition())
+
+    ctrl_left_.setCommand(cmd_);
+    ctrl_right_.setCommand(cmd_);
+    ctrl_left_.update(time, period);
+    ctrl_right_.update(time, period);
+    // if (has_feedforward_)
+    // {
+    //   ctrl_left_.joint_.setCommand(ctrl_left_.joint_.getCommand() + feedforward_);
+    //   ctrl_right_.joint_.setCommand(ctrl_right_.joint_.getCommand() + feedforward_);
+    // }
+
+  }
+
+  double SynController::getPosition()
+  {
+    return pos_;
+  }
+
+  void SynController::setCommand(double cmd)
+  {
+    ROS_INFO_STREAM(cmd);
+    cmd_ = cmd;
+  }
+
+  void SynController::setCommandCB(const std_msgs::Float64::ConstPtr &msg)
+  {
+    setCommand(msg->data);
+  }
+
+} // namespace syn_controller
+
+PLUGINLIB_EXPORT_CLASS(syn_controller::SynController, controller_interface::ControllerBase)
